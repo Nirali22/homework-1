fac7e2314843057456c9e89224562084ae8ec0f4
Nirali22 <43624168+Nirali22@users.noreply.github.com>
2019-02-06 19:04:05 -0500

    Update task12.py

--- task1/task12.py
+++ task1/task12.py
@@ -1,5 +1,5 @@
 import pandas as pd
-inp=pd.read_csv('input.txt',sep=',',encoding='UTF-16',engine='python',header=None,escapechar='\\')
+inp=pd.read_csv('task1/input.txt',sep=',',encoding='UTF-16',engine='python',header=None,escapechar='\\')
 new_header=inp.iloc[0]
 inp=inp[1:]
 inp.columns=new_header
