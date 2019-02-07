53eb7969a1c67fce65bfd7fa517994522fe11931
Nirali22 <43624168+Nirali22@users.noreply.github.com>
2019-02-06 19:08:21 -0500

    Update task12.py

--- task1/task12.py
+++ task1/task12.py
@@ -3,5 +3,5 @@ inp=pd.read_csv('task1/input.txt',sep=',',encoding='UTF-16',engine='python',head
 new_header=inp.iloc[0]
 inp=inp[1:]
 inp.columns=new_header
-inp.set_index('Iyear',inplace=True)
+inp.set_index('year',inplace=True)
 pop_2010=pd.to_numeric(inp['2010'],errors='coerce')
